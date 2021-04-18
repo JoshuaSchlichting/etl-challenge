b859f0d8ac8260b6f8a3d64124fcbcc08d9cbddb
Joshua Schlichting <8153552-RedGrinGrumble@users.noreply.gitlab.com>
2021-04-18 19:19:28 -0400

    Added logic to call FileMerger

--- src/main.py
+++ src/main.py
@@ -18,7 +18,19 @@ class EtlScript:
 
     def run(self):
         # Your code starts here.
-        pass
+
+        # If this looks messy, just know I only have these imports placed here 
+        # because I want to be compliant with the "# Your code starts here." above
+        from file_merger import FileMerger
+        merger = FileMerger(logger=logging.getLogger('FileMerger'))
+        merged_file_path = merger.merge_files(
+            header_file_path=self.header_file,
+            data_file_path=self.data_file,
+            out_file=self.out_file
+        )
+        logger.info(f'Merged file created: {merged_file_path}')
+        self.load_file_to_database(file_path=merged_file_path)
+
 
 if __name__ == "__main__":
     EtlScript().run()
