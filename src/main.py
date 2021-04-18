be09ebbb343dbda40b53c4c957c5b632cb16ee09
Joshua Schlichting <8153552-RedGrinGrumble@users.noreply.gitlab.com>
2021-04-18 19:19:56 -0400

    Fixed relative paths in EtlScript's __init__()

--- src/main.py
+++ src/main.py
@@ -9,9 +9,9 @@ logger = logging.getLogger(__name__)
 class EtlScript:
     def __init__(self):
         self.database_conn = Database("acme")
-        self.header_file = "./headers.txt"
-        self.data_file = "./data.csv"
-        self.out_file = "./output.csv"
+        self.header_file = "../headers.txt"
+        self.data_file = "../data.csv"
+        self.out_file = "../output.csv"
 
     def load_file_to_database(self, file_path: str):
         self.database_conn.load_file(file_path)
